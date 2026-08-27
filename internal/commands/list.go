b1bbb91fb095e1bf340bf5c08b3cbaaa3a5957ab
miladbeigi <milad.be@gmail.com>
2026-08-27 00:09:11 -0400

    feat: list command to show secrets status

---
+++ internal/commands/list.go
@@ -0,0 +1,62 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/milad/penhan/internal/secrets"
+	"github.com/milad/penhan/internal/state"
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all secrets and their status",
+	RunE:  runList,
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
+
+func runList(cmd *cobra.Command, args []string) error {
+	statePath := filepath.Join(".penhan", "state.json")
+	s, err := state.Load(statePath)
+	if err != nil {
+		s = state.NewState()
+	}
+
+	secretsDir := "secrets/"
+	err = filepath.Walk(secretsDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		ext := filepath.Ext(path)
+		if ext != ".yaml" && ext != ".yml" && ext != ".json" {
+			return nil
+		}
+
+		vaultPath := secrets.LocalToVault(path, secretsDir)
+		entry, exists := s.Secrets[vaultPath]
+
+		status := "new"
+		if exists {
+			status = entry.Status
+		}
+
+		fmt.Printf("  %-40s [%s]\n", path, status)
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
